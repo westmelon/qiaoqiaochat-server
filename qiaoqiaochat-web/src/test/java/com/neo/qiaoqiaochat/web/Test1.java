ae4c51f57bfc8ef4ad9cb5b64fcff78f766bba74
linyi <liny6407@joyowo.com>
2020-12-28 17:42:51 +0800

    git test

---
+++ qiaoqiaochat-web/src/test/java/com/neo/qiaoqiaochat/web/Test1.java
@@ -0,0 +1,13 @@
+package com.neo.qiaoqiaochat.web;
+
+/**
+ * @author linyi 2020-12-28
+ */
+public class Test1 {
+
+    //
+    private String
+            aa;
+
+
+}
---
+++ qiaoqiaochat-web/src/test/java/com/neo/qiaoqiaochat/web/Test2.java
@@ -0,0 +1,11 @@
+package com.neo.qiaoqiaochat.web;
+
+/**
+ * @author linyi 2020-12-28
+ */
+public class Test2 {
+
+
+    private String
+            bb;
+}
